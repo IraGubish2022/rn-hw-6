056545df20f5562ea55a73c657a0faa42c470e34
IraGubish2022 <iragubish@gmail.com>
2023-04-18 16:51:02 +0300

    323r

---
+++ firebase/config.js
@@ -0,0 +1,21 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+{
+    "type": "service_account",
+    "project_id": "react-native-hw6",
+    "private_key_id": "da43c198b224daff3d06d8101368d34aa48a3ef4",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCwkGNzk3gzQucL\n8ppgYhO5tZVdShCqZ9YEMTja2FlwOxCPpARDYdrsGxpA0ykekKp9LfQOEXAlrGMW\ny7PF8YMaQ3/tFsKU/iBHFH/5DDh9Me0LOs5Hs1Pj/vdiIr/9vvtJhRobLOsjGgyC\nZnwcoUuxzhzHYQewh9eqySC9IawXas+hn0WnOYTfMHGk/IOCXox9lw5vBW0UY1BY\nHY4ty5cTwIieXp8zUTOpWLrm7Q55akppouK3zq7o4upTGGN/liYfdHs3gjU6bdQX\nbxzYgjetBXB4ZeoDw/Z/cPR4BccDTTzcxyryX547k74aa9aZSy/3soxqea0C4w1t\n1wXIDq29AgMBAAECggEAAIiPJFw/srfzBsA24y+oq5j22I9OuZUSunSDnXWl34bu\nJ+Dw+lsjVC9IxwVcTHFXcoVfBWPcGOEKc5kt890k7kFh849sCxMxecWAFXqJ+Sbb\ndns5AFJ0YG32xV8hgVzAWZqDV62tEDAV9ZblrRh3qBPdVmTepxPh/Owaozek03VH\n3m+7efIMwBUnmqeps2jVzvAkogj63MRGOmNSQVwZcDAyQ6GukyWo6SVsJBWLRI9z\nXvo28nz1LLve+XUo8xECv6uUPy1liPlblqn9M2owFAsWVNmCCKQWjeDVEkcjFO8p\nBMw3c6JT0HwJEldCT0nSPtP2T0T72sODtyA+On9wgQKBgQDVKNMQtQoRxS5IFeKi\n1L+B2yMGPEuCpnl2WlGua9St5FD+YJZV3YUNpLD2UzA7Sly2etWidxZ9em0AuWBZ\nsUCPDdLAj/NktWVxcnilmkAGrOuWXVuhypmiH7fiEDJPWDlgALRIOGAjLUNRZT/C\n0DRpiF4rEl29w8ePI5yeML597QKBgQDUDLUL/SQ03qgP8k+4gPf406W3zmDWEfBS\nq6GrKPC1GKKsFHpxoxHkVGLfEtobc/E439KFKZHXUz/8kcmc6D6ObkyAyaKUkM7E\n4PvP7uMrh6UA+aG0owcrCy7LJuj7dHYvqVhhoGzMwO68Yf9Gy4Fl6y61FcpR1eco\nIOh95+d1EQKBgDLrAJgiFtit2GNhzvSEi4aSeShZKNR5vdP2uUJIAdLUEkDhg6fZ\nNZpPt/HvZzsShVkVuIiJl/a7UuR0Y76pJ7nEXI6hFEv/RM83v/gXA6pUjiYqAOSC\nB/cBSqSWp93qPjJfmLETvmtIuabs37FCmPKIn638QiY9aSMZJ6VmEWr1AoGAMFe5\nSsNqZrdAP2pxrFGStuKhjDb7srIiROamE9xgjBjStUJx68hOguyOIectXEbhfqLg\nXsVsX1e2ZcFi4me8I0KYJGBTtWUSDqZwIiaHGdxxzLgcmo70SmbhQyVaxkD11MXZ\nxWAKjd+KTd5VkXFBzdeBLX2LQ37tlSNzfYjKJtECgYEAwGkumxLb0SthSs5yM/FN\n8ddH6hXzqXLHb0SFkVG+LQ2f4hR27FlGzYdBOGJlHf5m/meuNP8hhnxkxMYD3QPh\nEcgAR9O7Nmzmm6xEx3OlDXf27Yw6UoqdpgRsvv+jj8CKbNb+TWmg4kL13ZfnYNRd\nYNCWKNHXPzmlfVTz3rmOlt8=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-8ub8t@react-native-hw6.iam.gserviceaccount.com",
+    "client_id": "117755497123340574030",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-8ub8t%40react-native-hw6.iam.gserviceaccount.com"
+  }
+  
